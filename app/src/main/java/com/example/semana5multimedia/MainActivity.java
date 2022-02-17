025015ecb5f4b6b2aeb94c129c864893359b4d30
JoseLobos <88002545+JoseLobos@users.noreply.github.com>
2022-02-16 19:28:49 -0600

    codigo reproductor de audio

--- app/src/main/java/com/example/semana5multimedia/MainActivity.java
+++ app/src/main/java/com/example/semana5multimedia/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.semana5multimedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout Menu;
+    TabItem opciones1,opciones2,opciones3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Menu=findViewById(R.id.Menu);
+        opciones1=findViewById(R.id.inicio);
+        opciones2=findViewById(R.id.reprod_audio);
+        opciones3=findViewById(R.id.reprod_video);
+
+        Menu.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition()==0){
+                    Intent siguiente=new Intent(MainActivity.this,MainActivity.class);
+
+            }
+                if (tab.getPosition()==1){
+                    Intent siguiente=new Intent(MainActivity.this,ReproductorDeAudio.class);
+
+                }
+
+                if (tab.getPosition()==2){
+                    Intent siguiente=new Intent(MainActivity.this,ReproductorDeVideo.class);
+
+                }
+            }
+
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/semana5multimedia/ReproductorDeAudio.java
+++ app/src/main/java/com/example/semana5multimedia/ReproductorDeAudio.java
@@ -3,12 +3,48 @@ package com.example.semana5multimedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ReproductorDeAudio extends AppCompatActivity {
+public class ReproductorDeAudio extends AppCompatActivity implements
+        View.OnClickListener  {
+
+    Button iniciar, parar;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reproductor_de_audio);
+
+        iniciar = (Button)findViewById(R.id.button);
+        parar   = (Button)findViewById(R.id.button2);
+        mediaPlayer = MediaPlayer.create(this,R.raw.heart);
+
+        iniciar.setOnClickListener(this);
+        parar.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                play();
+                break;
+            case R.id.button2:
+                stop();
+                break;
+        }
+    }
+    private void play(){
+        mediaPlayer.start();
+    }
+    private void stop(){
+        mediaPlayer.stop();
     }
 }
+
